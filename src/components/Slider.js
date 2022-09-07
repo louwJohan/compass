fc5e2739c29b4e5e1287bf558970c1852c44d9b2
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-09-07 09:18:22 +0000

    Add images to carousel

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import styles from "../styles/Slider.module.css";
 
 const Slider = () => {
   return (
@@ -7,39 +8,37 @@ const Slider = () => {
       <Carousel fade>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
+            className="d-block carousel"
+            src="https://res.cloudinary.com/dxwuotiow/image/upload/v1662542183/pexels-alex-andrews-833192_hunouf.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Buy</h3>
+            <p>Find the perfect house</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
+            className="d-block carousel"
+            src="https://res.cloudinary.com/dxwuotiow/image/upload/v1662541988/pexels-terje-sollie-312029_czqzy4.jpg"
             alt="Second slide"
           />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Sell</h3>
+            <p>Sell your property with Compass</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            className="d-block carousel"
+            src="https://res.cloudinary.com/dxwuotiow/image/upload/v1662539258/pexels-pixabay-460695_scl55x.jpg"
             alt="Third slide"
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3>Rent</h3>
+            <p>Rent the place you always dreamed of.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
