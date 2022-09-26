d51fe2abb98ea5d6dbff4890aa0aceb9c0ef2787
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-09-26 07:48:35 +0000

    Add redirect to sell id user is not logged in

--- src/pages/listings/Sell.js
+++ src/pages/listings/Sell.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ListingCreateForm from "./ListingCreateForm";
+import { useCurrentUser } from "../../context/CurrentUserContext.js";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Sell = () => {
+  const currentUser = useCurrentUser();
+  const history = useHistory();
   return (
-    <div>
-      <ListingCreateForm />
-    </div>
+    <div>{currentUser ? <ListingCreateForm /> : history.push("/signin")}</div>
   );
 };
 
