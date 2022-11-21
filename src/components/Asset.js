5b11eb72da3c512bcca85eeadd34ee8971e49442
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-11-21 20:58:23 +0000

    Add style to spinner

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -5,7 +5,9 @@ import styles from "../styles/Asset.module.css";
 const Asset = ({ spinner, src, message, height = 120 }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
-      {spinner && <Spinner animation="border" />}
+      {spinner && (
+        <Spinner animation="border" style={{ width: "5rem", height: "5rem" }} />
+      )}
       {src && <img src={src} alt={message} height="200" />}
       {message && <p className="mt-4">{message}</p>}
     </div>
